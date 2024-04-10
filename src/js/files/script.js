c9a11ce4cc25a815912e53e681ec45773a5ab0e2
Ice-CubeXQ <dealplug72@gmail.com>
2024-04-10 16:58:47 +0300

    refactoring header and added search

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -2,3 +2,14 @@
 // import { isMobile } from "./functions.js";
 // import { formsModules } from "./forms/forms.js";
 
+const searchButton = document.querySelector(".user-bar__search");
+if (searchButton) {
+  const searchPanel = document.querySelector(".search-panel");
+  const searchCloseButton = searchPanel.querySelector("button");
+  searchButton.addEventListener("click", function (e) {
+    searchPanel.classList.add("_open");
+  });
+  searchCloseButton.addEventListener("click", function (e) {
+    searchPanel.classList.remove("_open");
+  });
+}
